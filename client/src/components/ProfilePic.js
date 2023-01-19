369b068060fe0b08ad35a2fde23426af76bd6c5a
Julien Faist <j.faist@googlemail.com>
2023-01-19 18:36:22 +0100

    changed default picture url

--- client/src/components/ProfilePic.js
+++ client/src/components/ProfilePic.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ProfilePic({ togglePopup, url, first, last }) {
     url =
         url ||
-        "https:/s3.amazonaws.com/spicedling/-E2SRd1Y-R4G67_JbXqfpMtcmerzTutu.png";
+        "https://s3.amazonaws.com/spicedling/-E2SRd1Y-R4G67_JbXqfpMtcmerzTutu.png";
 
     const altText = first + last;
 
